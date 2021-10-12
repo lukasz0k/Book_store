6f77c4688ef47e9065f4607f13019676fb75d8de
lukasz <lukasz0klepka@gmail.com>
2021-10-12 15:52:33 +0200

    Start book store project, create base tamplates and amin panel, create basic views

---
+++ store/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from .models import Category, Product
+
+# Create your tests here.
+class TestCategoriesModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django', slug='django')
+
+    def test_category_model_entry(self):
+        """Test Category model data insertion/types/fields attributes"""
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_entry_name(self):
+        """Test catey model defoult name"""
+        data = self.data1
+        self.assertEqual(str(data), 'django')
---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Category, Product
+
+# Create your views here.
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+
